7e0a985ea75abbe460b536f1ec8424bb46e2944f
santoshkumarpingali <74549219+santoshkumarpingali@users.noreply.github.com>
2021-08-25 12:29:23 +0530

    Create driver.py

---
+++ driver.py
@@ -0,0 +1,7 @@
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
