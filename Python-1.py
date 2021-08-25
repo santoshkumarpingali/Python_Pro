6a5483240bfb6d7f1ac2aab94d3cdbc77974b7dd
santoshkumarpingali <pingali1510@gmail.com>
2021-08-25 12:06:34 +0530

    add the file

---
+++ Python-1.py
@@ -0,0 +1,11 @@
+import os
+
+# define the name of the directory to be created
+path = "C:/temp"
+
+try:
+    os.mkdir(path)
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
